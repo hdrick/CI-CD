80e74fcdc0fde9cdd197031ab46b720ff3fae24b
hdrick <gonzaleshendwrick@gmail.com>
2024-08-24 14:28:17 +0800

    added build stages

--- Jenkins/build.groovy
+++ Jenkins/build.groovy
@@ -22,6 +22,30 @@ pipeline {
                 )
             }
         }
+
+        stage('Build Docker Image'){
+            steps{
+                dir('my-crud-be'){
+                    sh 'docker build -t my-crud-app .'
+                }
+            }
+        }
+
+        stage('Run Tests') {
+            steps {
+                dir('my-crud-be') {
+                    // Run your application tests here if needed
+                    sh 'docker run --rm my-crud-app ./run-tests.sh' // Adjust as needed
+                }
+            }
+        }
+
+        post {
+            always {
+                // Clean up actions if needed
+                echo 'Cleaning up...'
+            }
+        }
     }
 
 }
